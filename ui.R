09b5de9a6b875a689457bc99bdab925f15e4207d
Kayvon Tari <kaytari@uw.edu>
2019-06-03 22:26:34 -0700

    turned csv into a dataframe

---
+++ ui.R
@@ -0,0 +1,24 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(ggthemes)
+bcl <- read.csv("data/Crime_Data.csv", stringsAsFactors = FALSE)
+filtered <- bcl %>% filter(bcl$Crime.Subcategory == "HOMICIDE")
+filtered <- filtered[-c(1:37), ]
+
+report_number <- unlist(filtered[1])
+occurred_date <- unlist(filtered[2])
+occured_time <- unlist(filtered[3])
+reported_date <- unlist(filtered[4])
+reported_time <- unlist(filtered[5])
+crime_subcategory <- unlist(filtered[6])
+primary_offence_description <- unlist(filtered[7])
+precinct <- unlist(filtered[8])
+sector <- unlist(filtered[9])
+beat <- unlist(filtered[10])
+neighborhood <- unlist(filtered[11])
+
+filtered_df <- data.frame(report_number, occurred_date, occured_time, reported_date, reported_time, 
+                          crime_subcategory, primary_offence_description, precinct, sector, beat,
+                          neighborhood)
+
