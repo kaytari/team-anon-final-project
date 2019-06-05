3242f2d9c229d3ae4f5507ca2faa4ffc142d9a06
Kayvon Tari <kaytari@uw.edu>
2019-06-05 01:18:13 -0700

    changed input and subsetting

--- server.R
+++ server.R
@@ -20,49 +20,23 @@ sector <- unlist(filtered[9])
 beat <- unlist(filtered[10])
 neighborhood <- unlist(filtered[11])
 
-filtered_df <- data.frame(report_number, occurred_date, occured_time, reported_date, reported_time, 
-                          crime_subcategory, offence, precinct, sector, beat,
-                          neighborhood)
+filtered_df2 <- data.frame(report_number, occurred_date, occured_time, reported_date, reported_time, 
+                           crime_subcategory, offence, precinct, sector, beat,
+                           neighborhood)
 
-server <- function(input, output){
-  # creates table of data to use when plotting
-  get_count <- reactive({
-    df_summary <- filtered_df %>%
-      select(input$variable) %>%
-      table() 
-  })
-  # color palette of light blue to blue
-  pal <- reactive({ 
-    pal1 <- colorRampPalette(colors = c("lightblue", "blue"))(nrow(get_count()))
-  })
-  # creates bar graph of selected variable vs number of homicides
-  output$barGraph <- renderPlot({
-    par(mar = c(8, 4, 2, 2) + 1)
-    
-    end_point = 0.5 + nrow(get_count()) + nrow(get_count())-1
-    
-    barplot(get_count(), ylab = "Number of Homicides", 
-            col = pal(),
-            main = paste("homicides vs.", input$variable), xaxt = "n", space = 1)
-    
-    text(seq(1.5,end_point,by=2), par("usr")[3]-0.25, 
-         srt = 50, adj= 1, xpd = TRUE,
-         labels = paste(rownames(get_count())), cex=0.65)
-  })
+server <- function(input, output) {
   
   output$distPlot <- renderPlot({
     subset_df <- subset(filtered_df, filtered_df$occurred_date >= input$daterange[1] & filtered_df$occurred_date <= input$daterange[2])
+    subset_df <- subset(subset_df, subset_df$sector == input$sectorInput)
     View(subset_df)
-    
     ggplot(subset_df, aes(x = occured_time, fill = occured_time)) + geom_histogram(stat = 'count') +
       xlab("Hour of Day") + ylab("Amount of Homicides") + ggtitle("Graph of Homicides Commited at Corresponding Hours") 
     
   })
-  
   output$amounttt <- renderText( {
     subset_df <- subset(filtered_df, filtered_df$occurred_date >= input$daterange[1] & filtered_df$occurred_date <= input$daterange[2])
     paste("There have been", nrow(subset_df) ,"homicides in Seattle within your time frame")
   })
 }
-
 shinyServer(server)
--- ui.R
+++ ui.R
@@ -4,20 +4,17 @@ library(dplyr)
 library(ggthemes)
 library(lubridate)
 library(xts)
-#Loading in data and filtering out all cells that don't corerespond to homicides
-#Also removed events that occured before 1/1/09
 bcl <- read.csv("data/Crime_Data.csv", stringsAsFactors = FALSE)
 filtered <- bcl %>% filter(bcl$Crime.Subcategory == "HOMICIDE")
 filtered <- filtered[-c(1:37), ]
 
-##Flattiing out data set from list format into data frame
 report_number <- unlist(filtered[1])
 occurred_date <- unlist(filtered[2])
 occured_time <- unlist(filtered[3])
 reported_date <- unlist(filtered[4])
 reported_time <- unlist(filtered[5])
 crime_subcategory <- unlist(filtered[6])
-offence <- unlist(filtered[7])
+offence<- unlist(filtered[7])
 precinct <- unlist(filtered[8])
 sector <- unlist(filtered[9])
 beat <- unlist(filtered[10])
@@ -26,7 +23,7 @@ neighborhood <- unlist(filtered[11])
 filtered_df <- data.frame(report_number, occurred_date, occured_time, reported_date, reported_time, 
                           crime_subcategory, offence, precinct, sector, beat,
                           neighborhood)
-## Fixes how dates are stored in data frame
+
 holder <- (filtered_df$occurred_date)
 holder2 <- mdy(holder)
 
@@ -34,7 +31,6 @@ filtered_df$occurred_date <- holder2
 
 filtered_df$reported_date <- mdy(filtered_df$reported_date)
 
-# Rounds time to nearest hour and only takes first 2 numbers
 temp <- filtered_df$occured_time 
 temp <- signif(temp, digits = 2)
 temp2 <- mapply(function(x, y) paste0(rep(x, y), collapse = ""), 0, 4 - nchar(temp))
@@ -44,21 +40,16 @@ temp <- substr(temp, 1, 2)
 filtered_df$occured_time <- temp
 
 ui <- fluidPage(
-  titlePanel(title = h1("Homicide Data in Seattle")),
-  
   sidebarLayout(
     sidebarPanel(
+      h1("Hello :)"),
       ## Allows users to select which variable to examine homicide data over 
       ## Precinct, Location Sector, or Neighborhood
-      selectInput("variable",
-                  "Choose a variable:",
-                  choices = c("Type of Homicide" = "offence",
-                            "Precinct" = "precinct",
-                            "Sector" = "sector",
-                            "Neighborhood" = "neighborhood")
+      selectInput("sectorInput",
+                  "Choose a Sector:",
+                  choices = c("B", "C", "D", "E","F", "G", "J", "K", "L", "M", "N", "O", "Q", "R", "S", "U", "W")
       ),
       
-      ## Allows users to select a range of dates to examine how homicides occur over span of day
       dateRangeInput(
         inputId = "daterange",
         label = "Select the occured date range",
@@ -69,15 +60,13 @@ ui <- fluidPage(
         format = "yyyy/mm/dd",
         separator = "-"
       )
-   ),
-   
-    ## Displays Bar Graph, then histogram, the last graph
+    ),
     mainPanel(
       plotOutput("barGraph"),
       plotOutput("distPlot"),
       textOutput("amounttt")
+      
     )
   )
 )
-
 shinyUI(ui)
